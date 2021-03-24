f97f5a44c9619586ff6648d42a8769e0c4f7c8d9
saraga1 <57629883+saraga1@users.noreply.github.com>
2021-03-23 20:46:49 -0400

    Update ConnectionUtil.java
    change environment variable

--- src/main/java/dev/araga/utils/ConnectionUtil.java
+++ src/main/java/dev/araga/utils/ConnectionUtil.java
@@ -9,7 +9,7 @@ public class ConnectionUtil {
     public static Connection createConnection(){
 
         //String details = System.getenv("CONN_DETAILS");
-        String details ="jdbc:postgresql://35.237.39.104:5432/expenseReimbursement?user=Samuel&password=quest456";
+        String details =CONN_DETAILS;
 
         try{
             Connection conn = DriverManager.getConnection(details);
